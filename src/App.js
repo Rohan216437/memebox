569f8107c7bd563e865ec4d34523d133b26f4efa
Rohan Gupta <7039426154rg@gmail.com>
2024-08-09 23:16:23 +0530

    starting , initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter , Routes , Route , Link} from 'react-router-dom';
+import Daaru from './Components/Daaru'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div>
+      <Link to="/daaru">
+      <button>
+        Click here to go to this page
+      </button>
+      </Link>
+      <Link to="/">
+      <button>
+        Click here to go back to this page
+      </button>
+      </Link>
     </div>
+
+      <Routes>
+        <Route path='/daaru' element={<Daaru />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
