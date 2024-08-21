3ed61811d130c9ce2971999a8139c5d050dc7043
Rohan Gupta <7039426154rg@gmail.com>
2024-08-21 22:47:40 +0530

    meme folder created

--- src/Components/Daaru.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Daaru = () => {
-  return (
-    <div>Daaru</div>
-  )
-}
-
-export default Daaru
---
+++ src/Components/Meme/Daaru.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Daaru = () => {
+  return (
+    <div>
+      <a href="/">go back to home page</a>
+      <h1>Daaru page</h1>
+      <a href="/contact">go back to contact page</a>
+    </div>
+  )
+}
+
+export default Daaru
